f1cf71c02866d0bcc4f2b6b7e8885822f2f9e11e
chengdan <17611136390@163.com>
2020-06-01 21:11:50 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img src="../assets/images/1.jpg" alt="">
+    <img src="../assets/images/2.jpg" alt="">
+    <img src="../assets/images/3.jpg" alt="">
+    <img src="../assets/images/4.jpg" alt="">
+    <img src="../assets/images/5.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+
+</script>
+<style>
+  @import url('../assets/css/style.min.css');
+</style>
